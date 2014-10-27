79b7c010496fc1021f4b7fc2b671bc61847464ff
opensoars <sam_1700@hotmail.com>
2014-10-27 20:54:48 +0100

    Got rid of spaces in Chrome extension JS

--- extensions/chrome/js/background.js
+++ extensions/chrome/js/background.js
@@ -7,16 +7,16 @@ var HTTP_URL = 'http://localhost:3332';
  * @return  {bool}    Whether request could be made or not
  */ 
 function checkIfUp(url, cb){
-	var req = new XMLHttpRequest();
+  var req = new XMLHttpRequest();
 
-	req.onreadystatechange = function (){
-		if(this.readyState === 4)
-			if(this.response === 'succes') return cb(true);
-			else return cb(false);
-	};
+  req.onreadystatechange = function (){
+    if(this.readyState === 4)
+      if(this.response === 'succes') return cb(true);
+      else return cb(false);
+  };
 
-	req.open('GET', url, true);
-	req.send();
+  req.open('GET', url, true);
+  req.send();
 }
 
 
@@ -27,37 +27,37 @@ function checkIfUp(url, cb){
 
  */
 function requestDownload(videoId, cb){
-	var req = new XMLHttpRequest();
+  var req = new XMLHttpRequest();
 
-	req.onreadystatechange = function (){
-		if(this.readyState === 4 && this.status === 200)
-			return cb(null);
-		
-		else if(this.readyState === 4 && this.status !== 200)
-			return cb('status !== 200');
-	};
+  req.onreadystatechange = function (){
+    if(this.readyState === 4 && this.status === 200)
+      return cb(null);
+    
+    else if(this.readyState === 4 && this.status !== 200)
+      return cb('status !== 200');
+  };
 
-	req.open('GET', HTTP_URL + '/requestDownload?v=' + videoId, true);
-	req.send();
+  req.open('GET', HTTP_URL + '/requestDownload?v=' + videoId, true);
+  req.send();
 }
 
 chrome.runtime.onConnect.addListener(function(port) {
 
-	if(port.name === 'ping')
-		port.onMessage.addListener(function() {
-			checkIfUp(HTTP_URL + '/ping', function (isUp){
-				port.postMessage({isUp: isUp});
-			});
-		});
-	
-	else if(port.name === 'requestDownload')
-		port.onMessage.addListener(function(msg){
-			if(msg.v)
-				requestDownload(msg.v, function (err){
-					if(err) return port.postMessage({status: 'failed', err: err});
-					port.postMessage({status: 'succes'});
-				});
-		});
+  if(port.name === 'ping')
+    port.onMessage.addListener(function() {
+      checkIfUp(HTTP_URL + '/ping', function (isUp){
+        port.postMessage({isUp: isUp});
+      });
+    });
+  
+  else if(port.name === 'requestDownload')
+    port.onMessage.addListener(function(msg){
+      if(msg.v)
+        requestDownload(msg.v, function (err){
+          if(err) return port.postMessage({status: 'failed', err: err});
+          port.postMessage({status: 'succes'});
+        });
+    });
 
 });
 
--- extensions/chrome/js/injected.js
+++ extensions/chrome/js/injected.js
@@ -6,7 +6,7 @@ if(window.location.href.indexOf('youtube.com/watch?') !== -1){
  * HTML/CSS to be injected
  */
 var dlBtnHTML = '<button id="dlBtn" class="yt-uix-button'
-	+ ' yt-uix-button-primary yt-uix-button-size-default">Download</button>';
+  + ' yt-uix-button-primary yt-uix-button-size-default">Download</button>';
 
 var sliderHTML = '<div id="progCont"><div id="progSlider"></div></div>';
 
@@ -20,14 +20,14 @@ var sliderHTML = '<div id="progCont"><div id="progSlider"></div></div>';
  * @return videoId {string}  Extracted video id
  */
 function getVideoId(url){
-	var q = url.split('/watch?')[1], qs = q.split('&');
+  var q = url.split('/watch?')[1], qs = q.split('&');
 
-	for(var i=0, videoId = ''; i<qs.length; i+=1){
-		var singleQ = qs[i];
-		if(singleQ.indexOf('v=') !== -1 && singleQ.charAt(0) === 'v')
-			videoId = singleQ.split('v=')[1];
-	}
-	return videoId;
+  for(var i=0, videoId = ''; i<qs.length; i+=1){
+    var singleQ = qs[i];
+    if(singleQ.indexOf('v=') !== -1 && singleQ.charAt(0) === 'v')
+      videoId = singleQ.split('v=')[1];
+  }
+  return videoId;
 }
 
 
@@ -39,12 +39,12 @@ function getVideoId(url){
 var pingPort = chrome.runtime.connect({name: 'ping'});
 
 pingPort.onMessage.addListener(function (msg){
-	msg = msg || {};
-	if(msg.isUp){
-		stopPing = true;
-		pingPort.disconnect();
-		init();
-	}
+  msg = msg || {};
+  if(msg.isUp){
+    stopPing = true;
+    pingPort.disconnect();
+    init();
+  }
 });
 
 
@@ -55,12 +55,12 @@ pingPort.onMessage.addListener(function (msg){
 var pingTimeout = 1000, stopPing = false;
 
 (function runPing(){
-	if(stopPing) return false;
+  if(stopPing) return false;
 
-	pingPort.postMessage();
+  pingPort.postMessage();
 
-	pingTimeout = pingTimeout * 1.3;
-	setTimeout(runPing, Math.round(pingTimeout));
+  pingTimeout = pingTimeout * 1.3;
+  setTimeout(runPing, Math.round(pingTimeout));
 }());
 
 setTimeout(function (){ stopPing = true; }, 60000);
@@ -70,13 +70,13 @@ setTimeout(function (){ stopPing = true; }, 60000);
  * Globals
  */
 var SOCKET_URL = 'ws://localhost:3334',
-		VIDEO_ID = getVideoId(window.location.href);
+    VIDEO_ID = getVideoId(window.location.href);
 
 var BTN_CONTAINER = document.getElementById('yt-masthead-signin');
 
 // If user is logged in, container changes ID
 if(!BTN_CONTAINER)
-	BTN_CONTAINER = document.getElementById('yt-masthead-user');
+  BTN_CONTAINER = document.getElementById('yt-masthead-user');
 
 
 /** Main Prodl inDOM application
@@ -87,153 +87,153 @@ if(!BTN_CONTAINER)
  */
 function init(){
 
-	var reqDlPort = chrome.runtime.connect({name: 'requestDownload'});
+  var reqDlPort = chrome.runtime.connect({name: 'requestDownload'});
 
-	reqDlPort.onMessage.addListener(function (msg){
-		msg.status = msg.status || '';
+  reqDlPort.onMessage.addListener(function (msg){
+    msg.status = msg.status || '';
 
-		if(msg.status === 'succes')
-			dlBtn.set({ t: 'Download has started successfuly!' });
-		else dlBtn.set({ c: 'red', t: 'Download could not be started, is Prodl running?' });
-	});
+    if(msg.status === 'succes')
+      dlBtn.set({ t: 'Download has started successfuly!' });
+    else dlBtn.set({ c: 'red', t: 'Download could not be started, is Prodl running?' });
+  });
 
-	BTN_CONTAINER.innerHTML += dlBtnHTML + sliderHTML;
+  BTN_CONTAINER.innerHTML += dlBtnHTML + sliderHTML;
 
-	var dlBtn = document.getElementById('dlBtn');
+  var dlBtn = document.getElementById('dlBtn');
 
-	dlBtn.set = function (o){
-		o = o || {};
+  dlBtn.set = function (o){
+    o = o || {};
 
-		if(o.c){ this.style.background = o.c; this.style.borderColor = o.c; }
-		if(o.t){ this.textContent = o.t;  }
-	};
+    if(o.c){ this.style.background = o.c; this.style.borderColor = o.c; }
+    if(o.t){ this.textContent = o.t;  }
+  };
 
-	var progCont = document.getElementById('progCont');
-	var progSlider = document.getElementById('progSlider');
+  var progCont = document.getElementById('progCont');
+  var progSlider = document.getElementById('progSlider');
 
-	progSlider.setWidth = function (w){
-		this.style.width = w + '%';
-	};
+  progSlider.setWidth = function (w){
+    this.style.width = w + '%';
+  };
 
-	var pinger = {
-		checkInterval: 3000,
-		maxInactive: 6000,
-		lastActive: new Date().getTime(),
+  var pinger = {
+    checkInterval: 3000,
+    maxInactive: 6000,
+    lastActive: new Date().getTime(),
 
-		stop: function (){ clearInterval(this.checkInterval); },
-		ping: function (){ this.lastActive = new Date().getTime(); },
-		start: function (){
-			var self = this;
-			self.ping();
-			self.checkInterval = setInterval(function (){
-				if( (new Date().getTime() - self.lastActive) > self.maxInactive){
-					dlBtn.set({ c: '#1b7fcc', t: 'No server response (is Prodl running?). Click to retry.' });
-					progSlider.setWidth(0);
-					self.stop();
-				}
-			}, self.checkInterval);
-		}
-	};
-
-
-	dlBtn.addEventListener('click', function (){
-		dlBtn.set({ c: 'orange', t: '. . .' });
-
-		reqDlPort.postMessage({ v: VIDEO_ID || getVideoId(window.location.href)} );
-		pinger.start();
-	}, true);
-
-	dlBtn.addEventListener('focus', function (){
-		setTimeout(function (){ dlBtn.blur(); }, 333);
-	});
+    stop: function (){ clearInterval(this.checkInterval); },
+    ping: function (){ this.lastActive = new Date().getTime(); },
+    start: function (){
+      var self = this;
+      self.ping();
+      self.checkInterval = setInterval(function (){
+        if( (new Date().getTime() - self.lastActive) > self.maxInactive){
+          dlBtn.set({ c: '#1b7fcc', t: 'No server response (is Prodl running?). Click to retry.' });
+          progSlider.setWidth(0);
+          self.stop();
+        }
+      }, self.checkInterval);
+    }
+  };
 
 
-	var ws = new WebSocket(SOCKET_URL);
+  dlBtn.addEventListener('click', function (){
+    dlBtn.set({ c: 'orange', t: '. . .' });
 
-	ws.sendJSON = function (json){ ws.send(JSON.stringify(json)); };
+    reqDlPort.postMessage({ v: VIDEO_ID || getVideoId(window.location.href)} );
+    pinger.start();
+  }, true);
 
-	ws.onopen = function (){
-		ws.sendJSON({type: 'setV', v: VIDEO_ID });
-	};
+  dlBtn.addEventListener('focus', function (){
+    setTimeout(function (){ dlBtn.blur(); }, 333);
+  });
 
-	ws.onmessage = function (d){
-		var data = d.data;
 
-		if(data.charAt(0) === '{') data = JSON.parse(data);
-		else return false;
-		if(!data.d) data.d = {};
+  var ws = new WebSocket(SOCKET_URL);
 
-		var type = data.type;
+  ws.sendJSON = function (json){ ws.send(JSON.stringify(json)); };
 
-		if(wsEvents[type] && typeof wsEvents[type] === 'function')
-			wsEvents[type](data.d);
-	};
+  ws.onopen = function (){
+    ws.sendJSON({type: 'setV', v: VIDEO_ID });
+  };
 
-	var wsEvents = {
+  ws.onmessage = function (d){
+    var data = d.data;
 
-		socketConnection: function (){
-			ws.sendJSON({type: 'setV', v: VIDEO_ID });
-		},
+    if(data.charAt(0) === '{') data = JSON.parse(data);
+    else return false;
+    if(!data.d) data.d = {};
 
-		dlNext: function (d){
-			pinger.ping();
-			dlBtn.set({ t: d.fn + ': step ' + d.next_i + ' / ' + d.fnCount });
-			progSlider.setWidth(0);
-		},
+    var type = data.type;
 
-		dlAbort: function (){
-			pinger.ping();
-			pinger.stop();
+    if(wsEvents[type] && typeof wsEvents[type] === 'function')
+      wsEvents[type](data.d);
+  };
 
-			dlBtn.set({c: 'red', t: '@' + dlBtn.textContent.split(':')[0] + ' aborted!'});
-			progSlider.setWidth(0);
+  var wsEvents = {
 
-			setTimeout(function (){
-				dlBtn.set({c: '#1b7fcc', t: 'Try again?'});
-				progSlider.setWidth(0);
-			}, 3000);
+    socketConnection: function (){
+      ws.sendJSON({type: 'setV', v: VIDEO_ID });
+    },
 
-		},
+    dlNext: function (d){
+      pinger.ping();
+      dlBtn.set({ t: d.fn + ': step ' + d.next_i + ' / ' + d.fnCount });
+      progSlider.setWidth(0);
+    },
 
-		dlRetry: function (d){
-			pinger.ping();
-			dlBtn.set({t: d.fn + ': step ' + '0' + ' / ' + d.fnCount});
-			progSlider.setWidth(0);
-		},
+    dlAbort: function (){
+      pinger.ping();
+      pinger.stop();
 
-		dlStart: function (){
-			pinger.ping();
-		},
+      dlBtn.set({c: 'red', t: '@' + dlBtn.textContent.split(':')[0] + ' aborted!'});
+      progSlider.setWidth(0);
 
-		dlFinish: function (d){
-			pinger.ping();
-			pinger.stop();
+      setTimeout(function (){
+        dlBtn.set({c: '#1b7fcc', t: 'Try again?'});
+        progSlider.setWidth(0);
+      }, 3000);
 
-			setTimeout(function (){
-				dlBtn.set({c: 'green', t: 'Download complete in: ' + d.timeTaken + ' ms'});
-				progSlider.setWidth(0);
-			}, 500);
-		},
+    },
 
-		conversionProgress: function (d){
-			pinger.ping();
+    dlRetry: function (d){
+      pinger.ping();
+      dlBtn.set({t: d.fn + ': step ' + '0' + ' / ' + d.fnCount});
+      progSlider.setWidth(0);
+    },
 
-			var percentageDone = 0;
-			try{ percentageDone = parseInt(d.percentage); }
-			catch(e){ console.log(e); }
+    dlStart: function (){
+      pinger.ping();
+    },
 
-			progSlider.setWidth(percentageDone);
-		},
+    dlFinish: function (d){
+      pinger.ping();
+      pinger.stop();
 
-		dlProgress: function (d){
-			pinger.ping();
-			progSlider.setWidth( (d.received / (d.clen / 100)) );
-		},
+      setTimeout(function (){
+        dlBtn.set({c: 'green', t: 'Download complete in: ' + d.timeTaken + ' ms'});
+        progSlider.setWidth(0);
+      }, 500);
+    },
 
-		deciphererRetry: function (){
-			pinger.ping();
-		}
-	};
+    conversionProgress: function (d){
+      pinger.ping();
+
+      var percentageDone = 0;
+      try{ percentageDone = parseInt(d.percentage); }
+      catch(e){ console.log(e); }
+
+      progSlider.setWidth(percentageDone);
+    },
+
+    dlProgress: function (d){
+      pinger.ping();
+      progSlider.setWidth( (d.received / (d.clen / 100)) );
+    },
+
+    deciphererRetry: function (){
+      pinger.ping();
+    }
+  };
 } // /init
 
 
