4c2a0c7021e3c3c1d107c03a8fb1e002b209efe5
unknown <sam_1700@hotmail.com>
2015-01-20 13:30:46 +0100

    More base structure added

--- index.js
+++ index.js
@@ -1,18 +1,38 @@
-/**
- * App modules to be globalized
- */
-var Ezlog = require('ezlog');
-
-
 (function (){
 
   /**
    *
-   * Global app namespace
+   * App namespacing/variables
    *
    */ 
   var app = {};
   process.app = app;
+
+  // App APIs namespace
+  app.apis = {};
+
+  // Downloads collection
+  app.downloads = [];
+
+
+  /**
+   *
+   * App modules
+   *
+   */
+  app.fs = require('fs');
+  app.cls = require('opensoars_cls');
+  app.Ezlog = require('ezlog');
+
+
+  /**
+   *
+   * App logger/cli styling
+   *
+   */
+  app.log = new app.Ezlog(['[app]', 'green']);
+  app.logErr = new app.Ezlog(['[app]', 'green'], ['red']);
+  app.logWarn = new app.Ezlog(['[app]', 'green'], ['yellow']);
 
 
   /**
@@ -22,13 +42,36 @@ var Ezlog = require('ezlog');
    */
 
   // Require user definable parameters
-  process.app.params = require('./params.json');
+  app.params = require('./params.json');
 
-  // HTTP server functionality
+  // Servers functionality
   app.http_server = require('./lib/servers/http.js');
-
-  // Websocket server functionality
   app.ws_server = require('./lib/servers/ws.js');
+
+  // APIS
+  app.apis.http = require('./lib/apis/http.js');
+  app.apis.ws = require('./lib/apis/http.js');
+
+  // Dump utility
+  app.Dump = require('./lib/dump');
+
+
+  /**
+   *
+   * Check/fix required params
+   *
+   */
+  if(!app.params.dl_dir)
+    return app.logErr('No dl_dir specified in params.json');
+  if(!app.params.http_port)
+    return app.logErr('No http_port specified in params.json');
+  if(!app.params.ws_port)
+    return app.logErr('No ws_port specified in params.json');
+
+  if(!app.params.max_retries)
+    app.params.max_retries = 10;
+  if(!app.params.log_all)
+    app.params.log_all = true;
 
 
   /**
@@ -37,13 +80,15 @@ var Ezlog = require('ezlog');
    *
    */
 
+  app.dump = new app.Dump({
+    dir: __dirname + '/dump/'
+  });
+
+
   // Initialize the http server
   app.http_server.instance = app.http_server.create(app.apis.http)
     .listen(process.app.params.http_port);
 
-
-  // Update global app namespace with scoped app variable
-  process.app = app;
 
 }());
 
