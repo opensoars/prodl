cf7df00fd03e8984f6ca79472b1d3f78ddd9f570
unknown <sam_1700@hotmail.com>
2015-01-21 10:18:37 +0100

    Better isApiCall

--- index.js
+++ index.js
@@ -8,19 +8,21 @@
   var app = {};
   process.app = app;
 
-  // App APIs namespace
-  app.apis = {};
 
+  app.api = {
+    http: {
+      url: '/api',
+      url_re: /^\/api\/?/
+    }
+  };
 
-  /**
-   *
-   * App modules
-   *
-   */
-  app.fs = require('fs');
-  app.qs = require('querystring');
-  app.cls = require('opensoars_cls');
-  app.Ezlog = require('ezlog');
+  app.modules = {
+    fs: require('fs'),
+    qs: require('querystring'),
+    http: require('http'),
+    cls: require('opensoars_cls'),
+    Ezlog: require('ezlog')
+  };
 
 
   /**
@@ -28,9 +30,9 @@
    * App logger/cli styling
    *
    */
-  app.log = new app.Ezlog(['[app]', 'green']);
-  app.logErr = new app.Ezlog(['[app]', 'green'], ['red']);
-  app.logWarn = new app.Ezlog(['[app]', 'green'], ['yellow']);
+  app.log = new app.modules.Ezlog(['[app]', 'green']);
+  app.logErr = new app.modules.Ezlog(['[app]', 'green'], ['red']);
+  app.logWarn = new app.modules.Ezlog(['[app]', 'green'], ['yellow']);
 
 
   /**
@@ -39,19 +41,13 @@
    *
    */
 
-  // Require user definable parameters
-  app.params = require('./params.json');
-
   // Downloads collection
   app.downloads = require('./lib/collections/downloads.js');
 
-  // Servers functionality
-  app.http_server = require('./lib/servers/http.js');
-  app.ws_server = require('./lib/servers/ws.js');
 
-  // APIS
-  app.apis.http = require('./lib/apis/http.js');
-  app.apis.ws = require('./lib/apis/http.js');
+  // Servers functionality
+  app.http = require('./lib/servers/http/');
+  app.ws = require('./lib/servers/ws');
 
   // Dump utility
   app.Dump = require('./lib/dump');
@@ -59,9 +55,11 @@
 
   /**
    *
-   * Check/fix required params
+   * Get/check/fix user definable parameters
    *
    */
+  app.params = require('./params.json');
+
   if(!app.params.dl_dir)
     return app.logErr('No dl_dir specified in params.json');
   if(!app.params.app_port)
@@ -89,8 +87,8 @@
 
 
   // Initialize the http server
-  app.http_server.instance = app.http_server.create(app.apis.http)
-    .listen(process.app.params.http_server_port);
+  app.http.server = app.http.create(app.http.listener)
+    .listen(app.params.http_server_port);
 
 
 }());
