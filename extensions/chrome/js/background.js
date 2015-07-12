53e3207b729ca59d16d780caaaa1424c83d83ad8
unknown <sam_1700@hotmail.com>
2015-07-12 17:34:47 +0200

    background.js refactor, clean code + comments

--- extensions/chrome/js/background.js
+++ extensions/chrome/js/background.js
@@ -1,21 +1,60 @@
-chrome.runtime.onMessage.addListener(function (req, sender, sendRes){
+/**
+ * Top level application namespace.
+ * @namespace
+ */
+var app = {};
 
-  console.log(req);
-
-  var http_req = new XMLHttpRequest();
-
-  http_req.open('POST', 'http://localhost:3333/api/downloads/' + req.v);
-
-  http_req.onreadystatechange = function (){
-    if(this.readyState === 4){
-      console.log('req done');
-    }
+/**
+ * Application event handlers namespace
+ * @namespace
+ */
+app.handlers = {
+  onMsg: function (req, sender, sendRes){
+    app.helpers.sendMsg(req, sendRes)
   }
+};
 
-  http_req.send();
+/**
+ * Application helper functions namespace.
+ * @namespace
+ */
+app.helpers = {
+  /**
+   * Sends messages to the prodl api.
+   * Callback action: TBD
+   * @param {object} req - Message data from injected.js
+   * @param {function} sendRes - Sends message back to injected.js
+   */
+  sendMsg: function (req, sendRes){
+    var http_req = new XMLHttpRequest();
 
-  sendRes({
-    hello: 'world'
-  })
+    http_req.open('POST', 'http://localhost:3333/api/downloads/' + req.v);
 
-});
+    http_req.onreadystatechange = function (){
+      if(this.readyState === 4){
+        console.log('req done');
+      }
+    };
+
+    http_req.send();
+
+    sendRes({});
+  },
+
+  /**
+   * Used to bind all application events.
+   */
+  addEventListeners: function (){
+    chrome.runtime.onMessage.addListener(app.handlers.onMsg);
+  }
+};
+
+/**
+ * Application initialization function.
+ */
+app.init = function (){
+  app.helpers.addEventListeners();
+};
+
+// Leggo
+app.init();
