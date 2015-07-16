64e767166c8315a5692b45eb6d53f19abb266e16
unknown <sam_1700@hotmail.com>
2015-07-16 14:19:37 +0200

    index2.js->index.js, index.js->index_old.js (u never know..)

--- index.js
+++ index.js
@@ -1,131 +1,142 @@
-/*
-(function (){
+/**
+ * Main application namespace
+ * @namespace
+ */
+var app = {};
 
-  var start_date = new Date().getTime(),
-      done_date = 0,
-      time_taken = 0;
+/** Bind root dirname to app */
+app.__dirname = __dirname;
 
-*/
-  /**
-   *
-   * App namespacing
-   *
-   */ 
-/*  var app = {};
-  process.app = app;*/
+app.start_time = new Date().getTime();
+app.ready_time = 0;
+app.time_taken = 0;
 
-
-  /**
-   *
-   * App modules
-   * 
-   */
-/*  app.modules = {
-    fs: require('fs'),
-    qs: require('querystring'),
-    http: require('http'),
-    cls: require('opensoars_cls'),
-    Ezlog: require('ezlog')
-  };*/
+/**
+ * Application module namespace
+ * @namespace
+ */
+app.modules = {
+  fs: require('fs'),
+  qs: require('querystring'),
+  http: require('http'),
+  https: require('https'),
+  cls: require('opensoars_cls'),
+  Ezlog: require('ezlog'),
+  f_: require('f_'),
+  spawn: require('child_process').spawn,
+  os: require('os')
+};
 
 
-  /**
-   *
-   * Get/check/fix user definable parameters
-   *
-   */
-/*  app.params = require('./params.json');
+app.temp_dir = __dirname + '/temp/';
 
-  if(!app.params.dl_dir)
+/**
+ * HTTP API namespace
+ * @namespace
+ */
+app.http_api = {};
+app.http_api.base = '/api';
+
+app.static_api = {};
+app.static_api.dir = __dirname + '/public';
+
+/**
+ * Application libraries namespace
+ * @namespace
+ */
+app.libs = {};
+
+/**
+ * User defined paramaters namespace (params.json)
+ * @namespace
+ */
+app.params = (function () {
+  var p = require('./params.json');
+
+  if (!p.dl_dir) {
     throw 'No dl_dir specified in params.json';
-  if(!app.params.app_port)
+  }
+  if (!p.app_port) {
     throw 'No app_port specified in params.json';
-  if(!app.params.http_api_port)
+  }
+  if (!p.http_api_port) {
     throw 'No http_api_port specified in params.json';
-  if(!app.params.ws_port)
+  }
+  if (!p.ws_port) {
     throw 'No ws_port specified in params.json';
+  }
 
-  if(!app.params.max_retries)
-    app.params.max_retries = 10;
-  if(!app.params.log && app.params.log !== false)
-    app.params.log = true;
+  p.max_retries = p.max_retries ? p.max_retries : 10;
+  p.use_log = p.use_log === false ? false : true;
 
-*/
-  /**
-   *
-   * App variables
-   * 
-   */
-/*  app.api = {
-    http: {
-      url: '/api',
-      url_re: /^\/api\/?/
-    }
-  };
+  return p;
+}());
 
-  app.dump = {};
-*/
+// Add loggers to app namespace
+require('./lib/loggers')(app).forEach(function (logger){
+  app[logger.name] = logger.func;
+});
 
-  /**
-   *
-   * Setup all loggers
-   *
-   */
+// Create an app dump helper function
+app.libs.dump = require('./lib/utils/dump')(app);
+app.dump = app.libs.dump.create({
+  dir: __dirname + '/dump/'
+});
 
-  //
-  // FIX THE LOGGIN SHIT
-  //
-/*  process.logs = require('./lib/loggers');
-
-*/
-  /**
-   *
-   * App libraries
-   *
-   */
-/*  app.downloads = require('./lib/collections/downloads.js');
-
-  app.http = require('./lib/servers/http/');
-  app.ws = require('./lib/servers/ws');
-
-  app.Dump = require('./lib/dump');
-
-*/
-  /**
-   *
-   * Initialize
-   *
-   */
-
-/*  app.dump = new app.Dump({
-    dir: __dirname + '/dump/'
-  });
+// Require decipher swap function solutions and f_ decipherer task list
+app.libs.swap_solutions =
+    require('./lib/Decipherer/lib/swap_solutions.js')(app);
+app.libs.Decipherer = require('./lib/Decipherer')(app);
 
 
-  app.http.router.get('/downloads', function (req, res){
-    console.log('keke');
-  });
+// Require downloads collection and assign shorthand to app namespace
+app.libs.downloads = require('./lib/collections/downloads');
+app.downloads = app.libs.downloads;
 
-  app.http.router.get('/downloads/:id', function (req, res){
-    console.log('keke ID');
-  });
+// Require Download f_ task list constructor and
+// assign shorthand to app namespace
+app.libs.Download = require('./lib/constructors/Download')(app);
+app.Download = app.libs.Download;
+
+app.libs.cleanDir = require('./lib/utils/cleanDir')(app);
+
+////////////////////// Download fixture(s) \\\\\\\\\\\\\\\\\\\\\
+setTimeout(function (){
+/*  app.downloads.add(
+    app.modules.f_.setup(
+      new app.Download({v: '_QeZVUO0DP0'})
+    ).start()
+  );*/
+}, 500);
+////////////////////// Download fixture(s) \\\\\\\\\\\\\\\\\\\\\
 
 
-  // Start listening for HTTP requests
-  app.http.server = app.http.create(app.http.router.listener)
+app.libs.cleanDir('/temp');
+app.libs.cleanDir('/dump');
+
+
+// Require HTTP functionality
+app.libs.http = require('./lib/servers/http')(app);
+app.http_api.router = app.libs.http.router;
+app.http_api.handlers = app.libs.http.handlers;
+
+// Setup routes
+app.http_api.router
+  .get('/downloads', app.http_api.handlers.getAll)
+  .get('/downloads/:id', app.http_api.handlers.getById)
+  .post('/downloads/:v', app.http_api.handlers.postNew)
+  .delete('/downloads/:id', app.http_api.handlers.deleteById);
+
+// Create server and start listening
+app.http_api.server =
+  app.libs.http.create(app.http_api.router.listener)
     .listen(app.params.http_api_port);
 
-  if(app.params.log)
-    app.log('HTTP server listening at port: ' + app.params.http_api_port);
+app.http_api_log('Server listening at port ' + app.params.http_api_port);
 
 
-  // Startup info
-  done_date = new Date().getTime();
-  app.time_taken = done_date - start_date;
+// Start up/time notifier
+app.ready_time = new Date().getTime();
+app.time_taken = app.ready_time - app.start_time;
 
-  if(app.params.log)
-    app.log('Ready, launched in ' +  app.time_taken + ' ms');
-
-}());
-*/
-
+app.log('Ready for business, launch time: ' + app.time_taken + 'ms');
---
+++ index_old.js
@@ -0,0 +1,131 @@
+/*
+(function (){
+
+  var start_date = new Date().getTime(),
+      done_date = 0,
+      time_taken = 0;
+
+*/
+  /**
+   *
+   * App namespacing
+   *
+   */ 
+/*  var app = {};
+  process.app = app;*/
+
+
+  /**
+   *
+   * App modules
+   * 
+   */
+/*  app.modules = {
+    fs: require('fs'),
+    qs: require('querystring'),
+    http: require('http'),
+    cls: require('opensoars_cls'),
+    Ezlog: require('ezlog')
+  };*/
+
+
+  /**
+   *
+   * Get/check/fix user definable parameters
+   *
+   */
+/*  app.params = require('./params.json');
+
+  if(!app.params.dl_dir)
+    throw 'No dl_dir specified in params.json';
+  if(!app.params.app_port)
+    throw 'No app_port specified in params.json';
+  if(!app.params.http_api_port)
+    throw 'No http_api_port specified in params.json';
+  if(!app.params.ws_port)
+    throw 'No ws_port specified in params.json';
+
+  if(!app.params.max_retries)
+    app.params.max_retries = 10;
+  if(!app.params.log && app.params.log !== false)
+    app.params.log = true;
+
+*/
+  /**
+   *
+   * App variables
+   * 
+   */
+/*  app.api = {
+    http: {
+      url: '/api',
+      url_re: /^\/api\/?/
+    }
+  };
+
+  app.dump = {};
+*/
+
+  /**
+   *
+   * Setup all loggers
+   *
+   */
+
+  //
+  // FIX THE LOGGIN SHIT
+  //
+/*  process.logs = require('./lib/loggers');
+
+*/
+  /**
+   *
+   * App libraries
+   *
+   */
+/*  app.downloads = require('./lib/collections/downloads.js');
+
+  app.http = require('./lib/servers/http/');
+  app.ws = require('./lib/servers/ws');
+
+  app.Dump = require('./lib/dump');
+
+*/
+  /**
+   *
+   * Initialize
+   *
+   */
+
+/*  app.dump = new app.Dump({
+    dir: __dirname + '/dump/'
+  });
+
+
+  app.http.router.get('/downloads', function (req, res){
+    console.log('keke');
+  });
+
+  app.http.router.get('/downloads/:id', function (req, res){
+    console.log('keke ID');
+  });
+
+
+  // Start listening for HTTP requests
+  app.http.server = app.http.create(app.http.router.listener)
+    .listen(app.params.http_api_port);
+
+  if(app.params.log)
+    app.log('HTTP server listening at port: ' + app.params.http_api_port);
+
+
+  // Startup info
+  done_date = new Date().getTime();
+  app.time_taken = done_date - start_date;
+
+  if(app.params.log)
+    app.log('Ready, launched in ' +  app.time_taken + ' ms');
+
+}());
+*/
+
